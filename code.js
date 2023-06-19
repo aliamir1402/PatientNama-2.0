bed30054a6c285bba925ab3bcea60402cd2a63ef
Ali Amir Khawaja <80505091+aliamir1402@users.noreply.github.com>
2023-06-19 16:09:51 +0500

    Add files via upload

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
-
 var loginFunc = () => {
     var email = document.getElementById('email').value;
     var password = document.getElementById('password').value;
-    
+
     if (email && password) {
         if (email === "aliamirkhawaja1@gmail.com" && password === "12345") {
             window.location.href = "dashboard.html";
@@ -14,169 +13,166 @@ var loginFunc = () => {
     }
 };
 
-var medFunc=()=>{
-    var container=document.getElementById("tab");
-    var htmlContent='<tr><td><input type="text" name="medicine" placeholder="Med Name..."></td><td><input type="text" name="dose" placeholder="Dose Name..."></td><td><input type="text" name="time" placeholder="Times a Day..."></td><td><input type="text" name="days" placeholder="Days..."></td></tr>';
+var medFunc = () => {
+    var container = document.getElementById("tab");
+    var htmlContent = '<tr><td><input type="text" name="medicine" placeholder="Med Name..."></td><td><input type="text" name="dose" placeholder="Dose Name..."></td><td><input type="text" name="time" placeholder="Times a Day..."></td><td><input type="text" name="days" placeholder="Days..."></td></tr>';
     container.insertAdjacentHTML("beforeend", htmlContent);
 }
 
-var searchFunc=(event)=>{
+var searchFunc = (event) => {
     event.preventDefault();
-    var medicine=[];
-    var dose=[];
-    var time=[];
-    var days=[];
-    var flag=0;
-    var searchItem=document.getElementById("search").value;
-    
+    var medicine = [];
+    var dose = [];
+    var time = [];
+    var days = [];
+    var flag = 0;
+    var searchItem = document.getElementById("search").value;
+
     // Use Fetch API to read the text file
     fetch('data.txt')
-    .then(response => response.text())
-    .then(data => {
-        var line=data.split("\n");
-        for (var i=0;i<line.length;i++) {
-            if((line[i].split("|"))[0]==searchItem)
-            {
-                var contents=line[i].split("|");
-                var hnumber=contents[0];
-                var fname=contents[1];
-                var age=contents[2];
-                var cnic=contents[3];
-                var address=contents[4];
-                var cname=contents[5];
-                var mnumber=contents[6];
-                var selectedValue=contents[7];
-                var admitDate=contents[8];
-                var birthDate=contents[9];
-                var clinicalInfo=contents[10];
-                var examFindings=contents[11];
-                var cbc=contents[12];
-                var lfts=contents[13];
-                var electrolytes=contents[14];
-                var viralmarkers=contents[15];
-                var imaging=contents[16];
-                var others=contents[17];
-                var operatingNotes=contents[18];
-                var course_treatment=contents[19];
-                for (var j=20;j<(line.length-1);j+=4)
-                {
-                    medicine.push(contents[j]);
-                    dose.push(contents[j+1]);
-                    time.push(contents[j+2]);
-                    days.push(contents[j+3]);
-                }
-                var followInfo=contents[line.length-1];
-                var prompt=document.getElementById("prompt");
-                var search_data=document.getElementById("search-data");
-                search_data.style.display="block";
-                prompt.style.display="block";
-                prompt.innerHTML="<span>Data Found Successfully.</span>";
-                flag=1;
+        .then(response => response.text())
+        .then(data => {
+            var line = data.split("\n");
+            for (var i = 0; i < line.length; i++) {
+                if ((line[i].split("|"))[0] == searchItem) {
+                    var contents = line[i].split("|");
+                    var hnumber = contents[0];
+                    var fname = contents[1];
+                    var age = contents[2];
+                    var cnic = contents[3];
+                    var address = contents[4];
+                    var cname = contents[5];
+                    var mnumber = contents[6];
+                    var selectedValue = contents[7];
+                    var admitDate = contents[8];
+                    var birthDate = contents[9];
+                    var clinicalInfo = contents[10];
+                    var examFindings = contents[11];
+                    var cbc = contents[12];
+                    var lfts = contents[13];
+                    var electrolytes = contents[14];
+                    var viralmarkers = contents[15];
+                    var imaging = contents[16];
+                    var others = contents[17];
+                    var operatingNotes = contents[18];
+                    var course_treatment = contents[19];
+                    for (var j = 20; j < (line.length - 1); j += 4) {
+                        medicine.push(contents[j]);
+                        dose.push(contents[j + 1]);
+                        time.push(contents[j + 2]);
+                        days.push(contents[j + 3]);
+                    }
+                    var followInfo = contents[line.length - 1];
+                    var prompt = document.getElementById("prompt");
+                    var search_data = document.getElementById("search-data");
+                    search_data.style.display = "block";
+                    prompt.style.display = "block";
+                    prompt.innerHTML = "<span>Data Found Successfully.</span>";
+                    flag = 1;
 
-                document.getElementById("fullName").innerHTML=fname;
-                document.getElementById("cName").innerHTML=cname;
-                document.getElementById("Age").innerHTML=age;
-                document.getElementById("Gender").innerHTML=selectedValue;
-                document.getElementById("mobileNumber").innerHTML=mnumber;
-                document.getElementById("hospitalNumber").innerHTML=hnumber;
-                document.getElementById("Cnic").innerHTML=cnic;
-                document.getElementById("Address").innerHTML=address;
-                document.getElementById("AdmitDate").innerHTML=admitDate;
-                document.getElementById("BirthDate").innerHTML=birthDate;
-                document.getElementById("clinicalInfo").innerHTML=clinicalInfo;
-                document.getElementById("examFindings").innerHTML=examFindings;
-                document.getElementById("CBC").innerHTML=cbc;
-                document.getElementById("LFT").innerHTML=lfts;
-                document.getElementById("Electrolyte").innerHTML=electrolytes;
-                document.getElementById("ViralMarkers").innerHTML=viralmarkers;
-                document.getElementById("Imagings").innerHTML=imaging;
-                document.getElementById("OTHERs").innerHTML=others;
-                document.getElementById("operativeNotes").innerHTML=operatingNotes;
-                document.getElementById("treatmentNotes").innerHTML=course_treatment;
-                document.getElementById("followInstructions").innerHTML=followInfo;
-                const iter=((line.length-1)-20)/4;
-                for (var j=0;j<iter;j++)
-                {
-                    var htmlContentMed='<div class="dischargeMed"><span class="dm" style="margin-left: 200px;">'+medicine[j]+'</span><span class="dm">'+dose[j]+'</span><span class="dm">'+time[j]+'</span><span class="dm">'+days[j]+'</span></div>';
-                    document.getElementById("dismed").insertAdjacentHTML("beforeend",htmlContentMed);
+                    document.getElementById("fullName").innerHTML = fname;
+                    document.getElementById("cName").innerHTML = cname;
+                    document.getElementById("Age").innerHTML = age;
+                    document.getElementById("Gender").innerHTML = selectedValue;
+                    document.getElementById("mobileNumber").innerHTML = mnumber;
+                    document.getElementById("hospitalNumber").innerHTML = hnumber;
+                    document.getElementById("Cnic").innerHTML = cnic;
+                    document.getElementById("Address").innerHTML = address;
+                    document.getElementById("AdmitDate").innerHTML = admitDate;
+                    document.getElementById("BirthDate").innerHTML = birthDate;
+                    document.getElementById("clinicalInfo").innerHTML = clinicalInfo;
+                    document.getElementById("examFindings").innerHTML = examFindings;
+                    document.getElementById("CBC").innerHTML = cbc;
+                    document.getElementById("LFT").innerHTML = lfts;
+                    document.getElementById("Electrolyte").innerHTML = electrolytes;
+                    document.getElementById("ViralMarkers").innerHTML = viralmarkers;
+                    document.getElementById("Imagings").innerHTML = imaging;
+                    document.getElementById("OTHERs").innerHTML = others;
+                    document.getElementById("operativeNotes").innerHTML = operatingNotes;
+                    document.getElementById("treatmentNotes").innerHTML = course_treatment;
+                    document.getElementById("followInstructions").innerHTML = followInfo;
+                    const iter = ((line.length - 1) - 20) / 4;
+                    for (var j = 0; j < iter; j++) {
+                        var htmlContentMed = '<div class="dischargeMed"><span class="dm" style="margin-left: 200px;">' + medicine[j] + '</span><span class="dm">' + dose[j] + '</span><span class="dm">' + time[j] + '</span><span class="dm">' + days[j] + '</span></div>';
+                        document.getElementById("dismed").insertAdjacentHTML("beforeend", htmlContentMed);
+                    }
+                    break;
                 }
-                break;
             }
-        }
-        if (flag==0){
-            var prompt=document.getElementById("prompt");
-            prompt.style.display="block";
-            prompt.innerHTML="<span>Data Not Found.</span>";
-            document.getElementById("search-data").style.display="none";
-        }  
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+            if (flag == 0) {
+                var prompt = document.getElementById("prompt");
+                prompt.style.display = "block";
+                prompt.innerHTML = "<span>Data Not Found.</span>";
+                document.getElementById("search-data").style.display = "none";
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
 }
 
 
 function sendData(contents) {
     fetch('http://localhost:3000/appendData', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(contents)
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(contents)
         })
-      .then(response => {
-        if (response.ok) {
-          console.log('Data sent to server and appended to file!');
-        } else {
-          console.error('Failed to send data to server:', response.status);
-        }
-      })
-      .catch(error => {
-        console.error('Error sending data to server:', error);
-      });
+        .then(response => {
+            if (response.ok) {
+                console.log('Data sent to server and appended to file!');
+            } else {
+                console.error('Failed to send data to server:', response.status);
+            }
+        })
+        .catch(error => {
+            console.error('Error sending data to server:', error);
+        });
 }
 
 
-var add = (event) =>{
+var add = (event) => {
     event.preventDefault();
-    
-    var fname=document.getElementById("name").value;
-    var age=document.getElementById("age").value;
-    var hnumber=document.getElementById("hnumber").value;
-    var cnic=document.getElementById("cnic").value;
-    var address=document.getElementById("address").value;
-    var cname=document.getElementById("cname").value;
-    var mnumber=document.getElementById("mnumber").value;
+
+    var fname = document.getElementById("name").value;
+    var age = document.getElementById("age").value;
+    var hnumber = document.getElementById("hnumber").value;
+    var cnic = document.getElementById("cnic").value;
+    var address = document.getElementById("address").value;
+    var cname = document.getElementById("cname").value;
+    var mnumber = document.getElementById("mnumber").value;
     var genderInputs = document.querySelectorAll('.gender');
     for (var i = 0; i < genderInputs.length; i++) {
         if (genderInputs[i].checked) {
-        var selectedValue = genderInputs[i].value;
-    break; 
+            var selectedValue = genderInputs[i].value;
+            break;
         }
     }
-    var admitDate=document.getElementById("dt").value;
-    var birthDate=document.getElementById("birthday").value;
-    var clinicalInfo=document.getElementById("cl").value;
-    var examFindings=document.getElementById("efArea").value;
-    var cbc=document.getElementById("cbc").value;
-    var lfts=document.getElementById("lfts").value;
-    var electrolytes=document.getElementById("electrolytes").value;
-    var viralmarkers=document.getElementById("viralmarkers").value;
-    var imaging=document.getElementById("imaging").value;
-    var others=document.getElementById("others").value;
-    var operatingNotes=document.getElementById("ontxt").value;
-    var course_treatment=document.getElementById("course").value;
-    var followInfo=document.getElementById("followup").value;
+    var admitDate = document.getElementById("dt").value;
+    var birthDate = document.getElementById("birthday").value;
+    var clinicalInfo = document.getElementById("cl").value;
+    var examFindings = document.getElementById("efArea").value;
+    var cbc = document.getElementById("cbc").value;
+    var lfts = document.getElementById("lfts").value;
+    var electrolytes = document.getElementById("electrolytes").value;
+    var viralmarkers = document.getElementById("viralmarkers").value;
+    var imaging = document.getElementById("imaging").value;
+    var others = document.getElementById("others").value;
+    var operatingNotes = document.getElementById("ontxt").value;
+    var course_treatment = document.getElementById("course").value;
+    var followInfo = document.getElementById("followup").value;
 
-    var med=document.getElementsByClassName("medicine");
-    var d=document.getElementsByClassName("dose");
-    var t=document.getElementsByClassName("time");
-    var da=document.getElementsByClassName("days");
+    var med = document.getElementsByClassName("medicine");
+    var d = document.getElementsByClassName("dose");
+    var t = document.getElementsByClassName("time");
+    var da = document.getElementsByClassName("days");
 
-    var medicine=[];
-    var dose=[];
-    var time=[];
-    var days=[];
+    var medicine = [];
+    var dose = [];
+    var time = [];
+    var days = [];
 
     for (var i = 0; i < med.length; i++) {
         medicine.push(med[i]);
@@ -184,69 +180,70 @@ var add = (event) =>{
         time.push(t[i]);
         days.push(da[i]);
     }
-    
-    var prompt=document.getElementById("prompt");
-    prompt.style.display="block";
-    prompt.innerHTML="<span>Data Added Successfullly.</span>";
-    
-    
-    const data = hnumber+"|"+fname+"|"+age+"|"+cnic+"|"+address+"|"+cname+"|"+mnumber+"|"+
-    selectedValue+"|"+admitDate+"|"+birthDate+"|"+clinicalInfo+"|"+examFindings+"|"+
-    cbc+"|"+lfts+"|"+electrolytes+"|"+viralmarkers+"|"+imaging+"|"+others+"|"+
-    operatingNotes+"|"+course_treatment+"|";
 
-    for (var i=0; i<medicine.length;i++)
-    {
-        data+=medicine[i]+"|"+dose[i]+"|"+time[i]+"|"+days[i]+"|";
-    }
-    data+=followInfo;
-    
-    var prompt=document.getElementById("prompt");
-    prompt.style.display="block";
-    prompt.innerHTML="<span>Data Added Successfullly.</span>";
+    var prompt = document.getElementById("prompt");
+    prompt.style.display = "block";
+    prompt.innerHTML = "<span>Data Added Successfullly.</span>";
 
-    // Example usage
-    console.log("A");
-    const str = 'hnumber|fname|age|cnic|address|cname|mnumber|selectedValue|admitDate|birthDate|clinicalInfo|examFindings|cbc|lfts|electrolytes|viralmarkers|imaging|others|operatingNotes|course_treatment|medicine1|dose1|time1|days1|medicine2|dose2|time2|days2|followInfo';
+    var dataArray = [
+        hnumber,
+        fname,
+        age,
+        cnic,
+        address,
+        cname,
+        mnumber,
+        selectedValue,
+        admitDate,
+        birthDate,
+        clinicalInfo,
+        examFindings,
+        cbc,
+        lfts,
+        electrolytes,
+        viralmarkers,
+        imaging,
+        others,
+        operatingNotes,
+        course_treatment,
+        followInfo
+    ];
 
-    const dataArray = str.split('|');
+
     const obj = {
-    hnumber: dataArray[0],
-    fname: dataArray[1],
-    age: dataArray[2],
-    cnic: dataArray[3],
-    address: dataArray[4],
-    cname: dataArray[5],
-    mnumber: dataArray[6],
-    selectedValue: dataArray[7],
-    admitDate: dataArray[8],
-    birthDate: dataArray[9],
-    clinicalInfo: dataArray[10],
-    examFindings: dataArray[11],
-    cbc: dataArray[12],
-    lfts: dataArray[13],
-    electrolytes: dataArray[14],
-    viralmarkers: dataArray[15],
-    imaging: dataArray[16],
-    others: dataArray[17],
-    operatingNotes: dataArray[18],
-    course_treatment: dataArray[19],
+        hnumber: dataArray[0],
+        fname: dataArray[1],
+        age: dataArray[2],
+        cnic: dataArray[3],
+        address: dataArray[4],
+        cname: dataArray[5],
+        mnumber: dataArray[6],
+        selectedValue: dataArray[7],
+        admitDate: dataArray[8],
+        birthDate: dataArray[9],
+        clinicalInfo: dataArray[10],
+        examFindings: dataArray[11],
+        cbc: dataArray[12],
+        lfts: dataArray[13],
+        electrolytes: dataArray[14],
+        viralmarkers: dataArray[15],
+        imaging: dataArray[16],
+        others: dataArray[17],
+        operatingNotes: dataArray[18],
+        course_treatment: dataArray[19],
     };
 
-    const medicineCount = (dataArray.length - 20) / 4;
+    const medicineCount = (dataArray.length - 21) / 4;
     for (let i = 0; i < medicineCount; i++) {
-    const medicineIndex = 20 + i * 4;
-    const doseIndex = medicineIndex + 1;
-    const timeIndex = medicineIndex + 2;
-    const daysIndex = medicineIndex + 3;
 
-    obj['medicine' + (i + 1)] = dataArray[medicineIndex];
-    obj['dose' + (i + 1)] = dataArray[doseIndex];
-    obj['time' + (i + 1)] = dataArray[timeIndex];
-    obj['days' + (i + 1)] = dataArray[daysIndex];
+        obj['medicine' + (i + 1)] = medicine[i];
+        obj['dose' + (i + 1)] = dose[i];
+        obj['time' + (i + 1)] = time[i];
+        obj['days' + (i + 1)] = days[i];
     }
 
     obj.followInfo = dataArray[dataArray.length - 1];
+    obj.medicineCount = medicineCount;
 
     sendData(obj);
 }
--- main.js
+++ main.js
@@ -1,27 +1,33 @@
-const path=require("path");
+const path = require("path");
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const {app, BrowserWindow} = require('electron');
+const {
+    app,
+    BrowserWindow
+} = require('electron');
 
 
-function createMainWindow(){
-    const { screen } = require('electron')
+function createMainWindow() {
+    const {
+        screen
+    } = require('electron')
     const primaryDisplay = screen.getPrimaryDisplay()
-    const { width, height } = primaryDisplay.workAreaSize
+    const {
+        width,
+        height
+    } = primaryDisplay.workAreaSize
     const MainWindow = new BrowserWindow({
-        title:'PatientNama',
+        title: 'PatientNama',
         width,
         height,
         icon: 'logo.jpg'
     });
 
-    MainWindow.loadFile(path.join(__dirname,"add.html"));
+    MainWindow.loadFile(path.join(__dirname, "login.html"));
 }
 
-app.whenReady().then(()=>{
-    createMainWindow();
-
+function server() {
     const app = express();
     const port = 3000;
     app.use(bodyParser.json());
@@ -29,33 +35,45 @@ app.whenReady().then(()=>{
     console.log("A");
     console.log("B");
     app.post('/appendData', (req, res) => {
-      const data = req.body;
-      const result = JSON.stringify(data);
-      const obj = JSON.parse(result);
-      console.log("F: "+obj.name);
-      var x=obj.name+"|"+obj.age;
-    console.log("Data: "+x);
-    if (!data) {
-        console.log("No Data");
-        res.status(400).send('No data provided');
-        return;
-    }
-    console.log("C");
-    fs.appendFile('data.txt', x + '\n', 'utf8', (err) => {
-        if (err) {
-        console.error('Error appending to file:', err);
-        res.status(500).send('Error appending to file');
-        return;
+        const data = req.body;
+        const obj = JSON.parse(JSON.stringify(data));
+
+        var x = obj.hnumber + "|" + obj.fname + "|" + obj.age + "|" + obj.cnic + "|" + obj.address + "|" + obj.cname + "|" + obj.mnumber + "|" + obj.selectedValue + "|" +
+            obj.admitDate + "|" + obj.birthDate + "|" + obj.clinicalInfo + "|" + obj.examFindings + "|" + obj.cbc + "|" + obj.lfts + "|" + obj.electrolytes + "|" + obj.viralmarkers +
+            "|" + obj.imaging + "|" + obj.others + "|" + obj.operatingNotes + "|" + obj.course_treatment + "|";
+
+        for (var i = 0; i < obj.medicineCount; i++) {
+            x += obj['medicine' + (i + 1)] + "|" + obj['dose' + (i + 1)] + "|" + obj['time' + (i + 1)] + "|" + obj['days' + (i + 1)] + "|";
         }
-        res.status(200).send('Data appended to file');
-        console.log("Appending");
-        console.log("D");
-    });
+        x += obj.followInfo;
+
+        console.log("Data: " + x);
+        if (!data) {
+            console.log("No Data");
+            res.status(400).send('No data provided');
+            return;
+        }
+        console.log("C");
+        fs.appendFile('data.txt', x + '\n', 'utf8', (err) => {
+            if (err) {
+                console.error('Error appending to file:', err);
+                res.status(500).send('Error appending to file');
+                return;
+            }
+            res.status(200).send('Data appended to file');
+            console.log("Appending");
+            console.log("D");
+        });
     });
     console.log("E");
     app.listen(port, () => {
-    console.log(`Server listening on port ${port}`);
-    console.log("F");
+        console.log(`Server listening on port ${port}`);
+        console.log("F");
     });
+}
+
+
+app.whenReady().then(() => {
+    createMainWindow();
+    server();
 });
-    
